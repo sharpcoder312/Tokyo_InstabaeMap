6f5970c365b75a44436a00a9f511a851fab35e9a
sharpcoder312 <tjr50999@naver.com>
2021-05-17 23:18:41 +0900

    Update ver. Alpha1.0.0

--- app.js
+++ app.js
@@ -3,11 +3,93 @@ var map;
 function createMap () {
   var options = {
     center: { lat: 35.6751184, lng: 139.784247 },
-    zoom: 12
+    zoom: 12,
+    styles: [
+      {
+          "featureType": "administrative.country",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+        "featureType": "poi.highway",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "rgb(39, 116, 160)"
+            }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#7dcdcd"
+            }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#e0efef"
+            }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#1900ff"
+            },
+            {
+                "color": "#c0e8e8"
+            }
+        ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "labels.text",
+          "stylers": [
+              {
+                  "visibility": "on"
+              }
+          ]
+      }
+  ]
   };
 
   map = new google.maps.Map(document.getElementById('map'), options);
 
+
+  
   var input = document.getElementById('search');
   var searchBox = new google.maps.places.SearchBox(input);
 
@@ -15,7 +97,7 @@ function createMap () {
     searchBox.setBounds(map.getBounds());
   });
 
-  var markers = [];
+  var searchMarkers = [];
   
   searchBox.addListener('places_changed', function () {
     var places = searchBox.getPlaces();
@@ -23,18 +105,19 @@ function createMap () {
     if (places.length == 0)
       return;
 
-    markers.forEach(function (m) { m.setMap(null); });
-    markers = [];
+    searchMarkers.forEach(function (m) { m.setMap(null); });
+    searchMarkers = [];
 
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(p) {
       if (!p.geometry)
         return;
 
-      markers.push(new google.maps.Marker({
+      searchMarkers.push(new google.maps.Marker({
         map: map,
         title: p.name,
-        position: p.geometry.location
+        position: p.geometry.location,
+        icon: "./icons/marker1.png",
       }));
 
       if (p.geometry.viewport)
@@ -45,4 +128,57 @@ function createMap () {
     
     map.fitBounds(bounds);
   });
-}  
+
+    // Add Markers to Array
+    var markers = [{
+      location: {
+        lat: 35.6899868,
+        lng: 139.7047913
+      },
+      iconImage: "./icons/marker1.png",
+      content: `<div style='width:520px; height:520px;'>
+      <a style='display:inline-block; width:250px; height:250px;' href="https://www.instagram.com/p/CKr27nHLhy2/" target="_blank> <img style='width:250px; height:250px;' src="images/1.jpg"/> </a>
+      <img style='width:250px; height:250px;' src="images/2.jpg">
+      <img style='width:250px; height:250px;' src="images/3.jpg">
+      <img style='width:250px; height:250px;' src="images/4.jpg">
+      </div>`
+    },
+    {
+      location: {
+        lat: 35.647174,
+        lng: 139.7355045,
+      },
+      iconImage: "./icons/marker1.png"
+    },
+  ]
+
+  // Loop through marker
+  const gmarkers = [];
+  for (let i = 0; i < markers.length; i++) {
+    gmarkers.push(addMarker(markers[i]));
+  }
+
+  // Add Marker
+  function addMarker(property) {
+    var marker = new google.maps.Marker({
+      position: property.location,
+      map: map,
+      icon: property.iconImage
+    });
+
+    const detailWindow = new google.maps.InfoWindow({
+      content: property.content
+    });
+
+    marker.addListener("click", () => {
+      detailWindow.open(map, marker);
+    })
+    return marker
+  }
+  const markerCluster = new MarkerClusterer(map, gmarkers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
